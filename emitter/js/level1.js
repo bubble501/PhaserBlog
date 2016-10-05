3f2a95ae9a001c2fe1980febc9f49ada2487035a
Christopher Gallup <chris.gallup@hotmail.com>
2016-10-05 13:11:19 -0500

    Phaser emitter(blog)

---
+++ emitter/js/level1.js
@@ -0,0 +1,77 @@
+Game.Level1 = function(game){}; 
+// variables 
+var background;
+var controls = {};
+var player; 
+var enemies;
+var emitter;
+
+Game.Level1.prototype = {
+    create:function (game) {
+    // reference to the background png
+    background = this.game.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, "background");
+    // create player
+    player = this.add.sprite(this.world.centerX, this.world.centerY + 250, "playerShip");
+    player.anchor.setTo(0.5,0.5);
+    player.scale.setTo(0.4,0.4);
+    this.physics.arcade.enable(player);
+    // Keeps player on screen
+    player.body.collideWorldBounds = true;
+
+    // create enemy group
+    enemies = this.add.group(); 
+    enemies.enableBody = true; 
+    enemies.physicsBodyType = Phaser.Physics.ARCADE;
+    this.createEnemies(2);
+
+    // create player's ship particles
+    emitter = this.add.emitter(player.x, player.y + 35, 50);  
+    emitter.minParticleScale = 0.1; 
+    emitter.maxParticleScale = 1.4; 
+    emitter.minParticleSpeed.setTo(-30, 30); 
+    emitter.maxParticleSpeed.setTo(30, -30);
+    emitter.makeParticles("playerShipParticles");
+    emitter.start(false, 2000, 50);
+
+
+    // ship controls
+    controls = {
+        right: this.input.keyboard.addKey(Phaser.Keyboard.D),
+        left: this.input.keyboard.addKey(Phaser.Keyboard.A)
+    };
+    }, 
+    // Update funtion, desires to be called 60 time a second. 
+    update: function () {
+        // allows for particles to follow player ship
+        emitter.x = player.x;
+        // scrolls background
+        background.tilePosition.y += 2;
+        // When player removes finger from A or D 
+        // this will set the velocity back to zero 
+        // to stop the ship from moving
+        player.body.velocity.x = 0; 
+        // conditional check if key 
+        // is being pressed down (A or D keys)
+        if (controls.right.isDown) {
+            player.body.velocity.x += 275; 
+        }
+        if (controls.left.isDown) {
+            player.body.velocity.x -= 275; 
+        }
+    }, 
+    createEnemies: function(count) {
+    for(var y = 0; y < count; y++) {
+        for(var x = 0; x < 5; x++) {
+            var enemy = enemies.create(x*58,y*60,'enemyShip');
+            enemy.anchor.setTo(0.5,0.5);
+            enemy.scale.setTo(0.3,0.3);
+            enemy.body.moves = false;
+        }
+    }
+    enemies.x = 100; 
+    enemies.y = 50; 
+    //  moving the group, rather than individually.
+    var tween = this.add.tween(enemies).to( { x: this.game.world.width - 400 }, 7000, Phaser.Easing.Linear.None, true, 0, 10000, true);
+    tween.onRepeat.add(function() { enemies.y += 10 }, this);
+    }
+}
---
+++ emitter/js/preloader.js
@@ -0,0 +1,26 @@
+Game.Preloader = function(game) {
+    this.preloaderBar = null; 
+}; 
+
+Game.Preloader.prototype = {
+    preload:function() {
+        // setting for the preload bar
+        this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, "preloaderBar"); 
+        this.preloadBar.anchor.setTo(0.5,0.5); 
+        this.time.advancedTiming = true; 
+        // preload starfield png created in photoshop
+        // with a key of background
+        this.game.load.image("background", "assets/starfield.png");
+        // preload ship height 
+        // with a key of playerShip
+        this.load.image("playerShip", "assets/ship.png");  
+        // preload enemy ship
+        this.load.image("enemyShip", "assets/enemy.png"); 
+        // preload particles for player ship
+        this.load.image("playerShipParticles", "assets/blast.png"); 
+    }, 
+
+    create:function() {
+        this.state.start("Level1"); 
+    }
+}
